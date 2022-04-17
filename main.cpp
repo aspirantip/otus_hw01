fb978baea2ceda04973a5a8cba1e278d15f44352
Ivan Plotnikov <plotnikov@protonmail.com>
2022-04-18 00:52:18 +0300

    Add string: Hello OTUS

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@
 int main(int, char **) {
 	std::cout << "Version: " << version() << std::endl;
 	std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, OTUS!" << std::endl;
+
 	return 0;
 }
